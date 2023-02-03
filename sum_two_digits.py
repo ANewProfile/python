f67d48ff6e2322151039d8c3527d7cfd7146abec
Theo Chen <theochen16@gmail.com>
2023-02-03 14:39:27 -0500

    realtwodigitsum

--- sum_two_digits.py
+++ sum_two_digits.py
@@ -1,6 +1,5 @@
-two_digit_number = input('Enter a two digit number: ')
-if len(two_digit_number) == 2:
-    total = 0
-    for num in two_digit_number:
-        total += num
-    print(f'The sum of the two digits in your number is: {total}')
+two_digit_number = float(input('Enter a two digit number: '))
+total = 0
+for num in two_digit_number:
+    total += num
+print(f'The sum of the two digits in your number is: {total}')
