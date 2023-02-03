7f7d48759179d38b0252a8b230b43d432d2e8d49
Theo Chen <theochen16@gmail.com>
2023-02-03 14:35:25 -0500

    sumtwodigits_edit

--- sum_two_digits.py
+++ sum_two_digits.py
@@ -1,7 +1,6 @@
 two_digit_number = input('Enter a two digit number: ')
-if len(two_digit_number) == 2
-  total = 0
-   for num in two_digit_number:
+if len(two_digit_number) == 2:
+    total = 0
+    for num in two_digit_number:
         total += num
-
     print(f'The sum of the two digits in your number is: {total}')
