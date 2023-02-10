2ab00645b2b80617794c81073360d0e4ed9d4395
Theo Chen <theochen16@gmail.com>
2023-02-10 14:12:00 -0500

    bmi-calculator

---
+++ BMI-calc.py
@@ -0,0 +1,7 @@
+height = input("Enter your height(meters): ")
+weight = input("Enter your weight(kg): ")
+
+square_height = height * height
+bmi = square_height/weight
+
+print(f"Your BMI(Body Mass Index) is {bmi}")
