6b0e6286f15f962d4f98ab7dcae57fb550b9085f
Theo Chen <theochen16@gmail.com>
2023-02-10 14:06:14 -0500

    bruh so much for sum_two_digits.py

--- sum_two_digits.py
+++ sum_two_digits.py
@@ -1,5 +1,5 @@
-two_digit_number = input('Enter a two digit number: ')
+two_digit_number = input('Enter a number: ')
 total = 0
 for num in two_digit_number:
     total += int(num)
-print(f'The sum of the two digits in your number is: {total}')
+print(f'The sum of the digits in your number is: {total}')
