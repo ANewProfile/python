46e7bf9d89abcec9c77dafed04ec5b66d0cf31b5
Theo Chen <theochen16@gmail.com>
2023-02-13 11:14:30 -0500

    actual bmi nothing else was right

--- BMI-calc.py
+++ BMI-calc.py
@@ -2,6 +2,6 @@ height = float(input("Enter your height(meters): "))
 weight = float(input("Enter your weight(kg): "))
 
 square_height = height * height
-bmi = square_height/weight
+bmi = weight/square_height
 
 print(f"Your BMI(Body Mass Index) is {bmi}")
