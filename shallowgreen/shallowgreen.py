ac1796f6aca5a683766b454c11ff3f6733223c27
Theo Chen <theochen16@gmail.com>
2023-04-30 07:57:35 -0400

    made theo_board_analyzer better

--- shallowgreen/shallowgreen.py
+++ shallowgreen/shallowgreen.py
@@ -1,61 +1,37 @@
 from chess import *
+from dad_board_analyzer import *
 from theo_board_analyzer import *
 from player import *
 
 board = Board()
-player = LookAheadPlayer(TheoBoardAnalyzer, depth=2)
+dad = LookAheadPlayer(DadBoardAnalyzer, depth=2)
+theo = LookAheadPlayer(DadBoardAnalyzer, depth=2)
 move = 0
 
 while True:
     turn = Board.WHITE if (move % 2 == 0) else Board.BLACK
 
-    # if turn == Board.WHITE:  # white move
-    #     print(board)
-    #     q = [input("Which piece would you like to move? "),
-    #          input("Where would you like to move it? ")]
-    #     if piece_color(board.piece_at(q[0])) != Board.WHITE:
-    #         print("Please move a white piece.")
-    #     else:
-    #         try:
-    #             board = board.move_piece(q[0], q[1])
-    #             move += 1
-    #         except InvalidMoveException as e:
-    #             print(e)
-
-    # else:   # black move
-    #     try:
-    #         q = player.computer_turn(board, Board.BLACK)
-    #     except GameOverException as e:
-    #         print(str(e))
-    #         exit()
-    #     board = board.move_piece(*q)
-    #     print("COMPUTER MOVE:", *q)
-    #     move += 1
-
-
-    # computer is white
-
     if turn == Board.WHITE:  # white move
-        try:
-            q = player.computer_turn(board, Board.WHITE)
-        except GameOverException as e:
-            print(str(e))
-            exit()
-        board = board.move_piece(*q)
-        print("COMPUTER MOVE:", *q)
-        move += 1
-        
-
-    else:   # black move
         print(board)
         q = [input("Which piece would you like to move? "),
              input("Where would you like to move it? ")]
-        if piece_color(board.piece_at(q[0])) != Board.BLACK:
-            print("Please move a white piece.")
+        if len(q[0]) != 2 or len(q[1]) != 2 or \
+           piece_color(board.piece_at(q[0])) != Board.WHITE:
+            print("Please move a white piece to an empty space.")
         else:
             try:
                 board = board.move_piece(q[0], q[1])
                 move += 1
             except InvalidMoveException as e:
                 print(e)
-        
+
+    else:   # black move
+        try:
+            q = theo.computer_turn(board, Board.BLACK)
+        except GameOverException as e:
+            print(str(e))
+            exit()
+        print(board.piece_locations())
+        board = board.move_piece(*q)
+        print("COMPUTER MOVE:", *q)
+        move += 1
