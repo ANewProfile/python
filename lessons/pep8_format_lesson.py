b518d543eaa8cd45b3a4168aa20e0109376600cb
Theo Chen <theochen16@gmail.com>
2023-05-10 19:10:39 -0400

    new pep8 lesson

--- lessons/pep8_format_lesson.py
+++ lessons/pep8_format_lesson.py
@@ -13,13 +13,6 @@ class LineLen:
     ) -> None:
         print(f'Run: arg1: {argument1}')
 
-    # fixed!
-
-    def summary() -> None:
-        # any given long name should not take up more than 80 characters in a single line
-        # if it does, use [enter] key to make it multiple lines
-        pass
-
 
 class NamingConvention:
 
@@ -49,15 +42,6 @@ class NamingConvention:
         print('file_name.py')
         print('name.py')
 
-    # fixed!
-
-    def summary(self, case, constant_case, file_name) -> None:
-        case = 'snake_case'
-        case = 'snake_case_example'
-        constant_case = NamingConvention.CONSTANT
-        constant_case = 'all caps'
-        file_name = 'all lower, and snake case'
-
 
 class ClassNames:
 
@@ -72,15 +56,6 @@ class ClassNames:
 
     def fixed_class_name(self) -> None:
         print('class ClassNames()')
-
-    # fixed!
-
-    def summary(self) -> None:
-        # class names should be in Pascal Case
-        # not snake case or camel case
-        pass
-
-    # fun fact: exceptions should also be in Pascal Case
 
 
 class Spacing:
@@ -114,14 +89,73 @@ class Spacing:
             def example(self, c2) -> None:
                 print(c2)
 
-    # fixed!
-
-    def summary(self) -> None:
-        # top-level classes and functions should be two whitespaces apart
-        # methods should be one whitespace apart
-        pass
-
 
 class Imports:
+    def __init__(self) -> None:
+        pass
+
+    def loc(self):
+        print('The location of your imports \
+              should always be at the top, \
+              unless there is a specific reason not to.')
+        print('Even though you can, you should not \
+              import two distinct modules in the same line.')
+        print('Two similar modules or two modules from the same library \
+              can be imported in the same line \
+              (e.g. from os import path, stat).')
+        print('It is also okay to not import modules \
+              at the top of your code if you have a module doc string \
+              (the thing with triple quotations)')
+        print('DISCLAIMER: NEVER use wildcad imports \
+              (e.g. from os import *)!')
+        
+
+class Strings:
+    def __init__(self) -> None:
+        pass
+
+    def single_or_double(self) -> None:
+        print('There is actually NO specific \
+              type of quotation that you should use, \
+              as long as you always use the same one!')
+        print('You should wrap quotations if you can.')
+        print('(e.g. "QUOTE: \'hello\'")')
+        print('However, you should ALWAYS use double quotations\
+               when writing doc strings(triple quotations)')
+
+
+class Whitespaces:
+    def __init__(self) -> None:
+        pass
+
+    def function_calls_and_brackets(self) -> None:
+        print('There should be NO whitespaces in between\
+               brackets, function calls, and that kind of stuff')
+        print(f'Correct:\nfunc(arg[1])\n\nIncorrect:\nfunc ( arg [ 1 ])')
+
+    def variables(self) -> None:
+        print('There should be one whitespace between variable definitions, \
+              one whitespace beween +=, -=, +, and -, \
+              and no whitespaces between * and / symbols(usually).')
+        print('DISCLAIMER: There may be some exceptions.')
+        print('\nCorrect:\ni = i + 1\n\
+              i += 1\n\
+              x = x*2 - 1\n\
+              hypt2 = x*x + y*y\n\
+              c = (a+b) * (a-b)\n\n\
+              Incorrect:\ni=i+1\n\
+              i +=1\n\
+              x = x * 2-1\n\
+              hypt2= x * x + y * y\n\
+              c = (a + b) * (a - b)')
+    
+    def default_paramaters(self) -> None:
+        print('There should be NO whitespaces between the default of paramaters.')
+        print('\nCorrect:\ndef complex(real, imag=0.0):\n\
+                 return magic(r=real, i=imag)')
+        print('\nIncorrect:\ndef complex(real, imag = 0.0):\n\
+                 return magic(r = real, i= imag)')
+
+class InLineComments:
     def __init__(self) -> None:
         pass
