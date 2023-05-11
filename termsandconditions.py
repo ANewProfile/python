8528b9988094c63b863bf1394faa652effbc462e
Theo Chen <theochen16@gmail.com>
2023-05-11 18:00:55 -0400

    edit

--- termsandconditions.py
+++ termsandconditions.py
@@ -1,49 +1,45 @@
-from threading import Thread
+import asyncio
 import webbrowser
 import os
-import time
 
 input('Would you like to see the terms and conditions? ')
-
-done = False
-
 webbrowser.open_new_tab('file:///'+os.getcwd()+'/'+'termsandconditions.html')
 
 
-
-def one(timer=300):
+async def one(timer=15):
     while True:
         if timer > 0:
-            time.sleep(1)
+            await asyncio.sleep(1)
             timer -= 1
         else:
-            return
+            return True
 
 
-def two():
+async def two(done):
     trust = input('Do you agree to the terms and conditions([y]es or [n]o)? ')
     while True:
         try:
-            if trust.lower() == 'y':
-                if done is False:
-                    print('You lied!!!!!!!')
-                    return
-                else:
-                    print('Wow. I think I\'ve met the most trustworthy person on the internet.')
-                    return
-            elif trust.lower() == 'n':
+            if trust.lower() == 'n':
                 print('SERIOUSLY BRO!!!???')
                 return
-            else:
-                raise Exception('Not an answer')
+            elif trust.lower() == 'y':
+                    if done is True:
+                        print(
+                            'Wow. I think I\'ve met the most trustworthy\
+ person on the internet.')
+                        return
+                    else:
+                        print('You lied!!!!!!!')
+                        return
+                        
         except Exception as e:
             print(e)
 
 
+async def main():
+    task2 = asyncio.create_task(one())
+    task = asyncio.create_task(two(task2))
 
-t1 = Thread(target=one)
-t2 = Thread(target=two)
 
-t1.start()
-done = True
-t2.start()
+
+asyncio.run(main())
