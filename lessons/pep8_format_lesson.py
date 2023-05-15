b440b900d9245ba16d008e696e9db2f67db15f66
Theo Chen <theochen16@gmail.com>
2023-05-15 17:41:27 -0400

    finished pep8 formatting lesson

--- lessons/pep8_format_lesson.py
+++ lessons/pep8_format_lesson.py
@@ -94,7 +94,7 @@ class Imports:
     def __init__(self) -> None:
         pass
 
-    def loc(self):
+    def loc(self) -> None:
         print('The location of your imports \
               should always be at the top, \
               unless there is a specific reason not to.')
@@ -106,7 +106,7 @@ class Imports:
         print('It is also okay to not import modules \
               at the top of your code if you have a module doc string \
               (the thing with triple quotations)')
-        print('DISCLAIMER: NEVER use wildcad imports \
+        print('DISCLAIMER: NEVER use wildcard imports \
               (e.g. from os import *)!')
         
 
@@ -156,6 +156,86 @@ class Whitespaces:
         print('\nIncorrect:\ndef complex(real, imag = 0.0):\n\
                  return magic(r = real, i= imag)')
 
+
 class InLineComments:
     def __init__(self) -> None:
         pass
+
+    def rules(self):
+        print('There must be at least two spaces\
+               between the code and comment.')
+        print('There must be at least one space\
+               between the pound symbol in the comment')
+        print('If it makes sense, start the comment with a capital')
+    
+    def example(self) -> None:
+        COLOR = (0, 255, 0)  # This is correct
+
+        COLOR = (0, 255, 0) #this is incorrect
+
+
+class IsNone:
+    def __init__(self) -> None:
+        pass
+
+    def rules(self) -> None:
+        print('While checking if the variable "x" is equal to none, we could\
+               say `if x:`, but it\'s better practice to say `if x is None:`')
+        print('Use the keyword "is" instead of "=="\
+               when checking for bools and NoneType')
+        print('Also, use the keyword "is not" instead of "not is"!')
+    
+    def example(self) -> None:
+        print('x = None')
+        print('if x is None:')
+        print('    pass')
+        print('\n\n')
+        print('\#Correct')
+        print('if foo is not None:')
+        print('    pass')
+        print()
+        print('\#Incorrect')
+        print('if not foo is None:')
+        print('    pass')
+        print('\n\n')
+        print('Both work, but it\'s good practice to use the first one')
+
+
+class TryExcept:
+    def __init__(self) -> None:
+        pass
+
+    def rules(self) -> None:
+        print('When using try-except blocks,\
+               always except a specific error to be more specific')
+        print('If you would like to except multiple exceptions, use multiple except blocks')
+    
+    def example(self) -> None:
+        print('\#Correct')
+        print('try:')
+        print('    import platform_specific_module')
+        print('except ImportError:')
+        print('    platform_specific module = None')
+        print('\n\n')
+        print('\#Incorrect')
+        print('try:')
+        print('    import platform_specific_module')
+        print('except:')
+        print('    platform_specific_module = None')
+
+
+class StartsWith:
+    def __init__(self) -> None:
+        pass
+
+    def rules(self) -> None:
+        print('Always use the built-in "x.startswith(\'\')" or "endswith" function instead of slicing')
+
+    def example(self) -> None:
+        print('\#Correct')
+        print('if foo.startswith("bar"):')
+        print('    pass')
+        print('\n\n')
+        print('\#Incorrect')
+        print('if foo[:3] == "bar":')
+        print('    pass')
