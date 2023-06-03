e626c6f688ac0d35b06851d7e369aeeb33c812ec
Theo Chen <theochen16@gmail.com>
2023-06-03 08:33:40 -0400

    Finished? (text-based)

--- restaurant_tracker/main.py
+++ restaurant_tracker/main.py
@@ -1,3 +1,4 @@
+import requests
 import pymongo
 from pymongo import MongoClient
 
@@ -21,11 +22,18 @@ restaurantDB = client['restaurants'].entries
 # *Id: slugify(name) - Calculated
 # Link: String - User
 # *Rating: Float/Double - User
-# *Loc: Loc - User
+# *Loc: Loc - User/Calc
 # Type: Array/Tuple[String] - User
 # Fav_Dishes: Array/Tuple[String] - User
 # Notes: Array/Tuple[String] - User
 # *IsOpen: Boolean - Calculated [default True]
+
+
+class Loc:
+    def __init__(self, city, country, response) -> None:
+        self.city = city
+        self.country = country
+        self.lalo = (response['latitude'], response['longitude'])
 
 
 def slugify(text: str):
@@ -36,21 +44,25 @@ def slugify(text: str):
     return final
 
 
-def get_user_inputs():
+def get_user_inputs() -> dict:
     items = {}
 
+    # Restaurant Name
     name = input('Enter the name (return to cancel): ')
     if name == '':
         return None
     items['Name'] = name
 
+    # Restaurant ID
     id = slugify(name)
     items['_id'] = id
 
+    # Restaurant Link
     link = input('Enter the link (optional): ')
     if link != '':
         items['Link'] = link
     
+    # Restaurant Rating
     while True:
         try:
             rating = float(input('Enter the rating: '))
@@ -60,8 +72,16 @@ def get_user_inputs():
             print('Not a valid number.')
             continue
     
-    # XXX LOCATION - IMPLEMENT ME
+    # Restaurant Location
+    city = input('What city is the restaurant in? ').lower()
+    country = input('What country is the restaurant in? ').lower()
+    # response = 'https://api.radar.io/v1/geocode/forward?query=841+broadway+new+york'
+    url = f'https://api.radar.io/v1/geocode/forward?query={city},+{country}'
+    response = requests.get(url, headers={'Authorization': 'prj_live_sk_9f1c623207cfd14adabf5ed963d167f327e22df7'}).json()
+    loc = [Loc(city, country, addr) for addr in response["addresses"]][0]
+    items['Loc'] = loc
 
+    # Restaurant Type(s)
     types = []
     while True:
         type = input('Type of cuisine (return to cancel): ')
@@ -71,6 +91,7 @@ def get_user_inputs():
     if types != []:
         items['Type'] = types
     
+    # Restaurant Favorite Dish(es)
     dishes = []
     while True:
         dish = input('Fav Dish (return to cancel): ')
@@ -80,21 +101,32 @@ def get_user_inputs():
     if dishes != []:
         items['Fav_Dishes'] = dishes
     
+    # Restaurant Note(s)
     notes = []
     while True:
-        note = input('Type of cuisine (return to cancel): ')
+        note = input('Note (return to cancel): ')
         if note == '':
             break
         notes.append(note)
     if notes != []:
         items['Notes'] = notes
 
-    items['IsOpen'] = true
+    # Restaurant Open - Default True
+    items['IsOpen'] = True
 
 
     return items
 
 
-print(get_user_inputs())
+# print(get_user_inputs())
+entry = get_user_inputs()
+print('\n\n\n')
+confirm = input('Confirm [y/n]: ')
 
-# restaurantDB.insert_one(entry)
+if confirm.lower() == 'y':
+    # entry = get_user_inputs()
+    loc_object: Loc = entry['Loc']
+    entry['Loc'] = [loc_object.city, loc_object.country, loc_object.lalo]
+    restaurantDB.insert_one(entry)
+else:
+    print('Ok! Thanks!')
