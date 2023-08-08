eb415062d754e8e8566039d6323260c86eb21583
Theo Chen <theochen16@gmail.com>
2023-08-08 07:07:23 -0400

    Fixed: No comps & Repeating comps

--- wca_comp_scanner.py
+++ wca_comp_scanner.py
@@ -35,19 +35,34 @@ def get_opens(opens):
     return open_list
 
 
-html = get_data('https://www.worldcubeassociation.org/competitions?region=all&search=Mass&state=present&year=all+years&from_date=&to_date=&delegate=&show_registration_status=on&display=list')
-doc = BeautifulSoup(html, 'html.parser')
-list_group = doc.find(class_='list-group')
-# comp_list = soup.find(id='upcoming-comps', _class='col-md-12')
-num_comps = list_group.find('strong')
-# num_comps = comp_list.strong
-comps_html = list_group.find_all(class_='competition-link')
-locs_html = list_group.find_all(class_='location')
-open_html = list_group.find_all(class_='date')
-comps = name_comps(comps_html)
-locs = get_locs(locs_html)
-opens = get_opens(open_html)
-comps_locs = list(zip(locs, comps, opens))
+urls = ['https://www.worldcubeassociation.org/competitions?region=all&search=Massachusetts&state=present&year=all+years&from_date=&to_date=&delegate=&show_registration_status=on&display=list',
+        'https://www.worldcubeassociation.org/competitions?region=all&search=Rhode+Island&state=present&year=all+years&from_date=&to_date=&delegate=&show_registration_status=on&display=list',
+        'https://www.worldcubeassociation.org/competitions?region=all&search=New+Hampshire&state=present&year=all+years&from_date=&to_date=&delegate=&show_registration_status=on&display=list']
+
+comps_locs_list = []
+
+
+for url in urls:
+    html = get_data(url)
+    doc = BeautifulSoup(html, 'html.parser')
+    list_group = doc.find(class_='list-group')
+    # comp_list = soup.find(id='upcoming-comps', _class='col-md-12')
+    # num_comps = list_group.find('strong')
+    # num_comps = comp_list.strong
+    try:
+        comps_html = list_group.find_all(class_='competition-link')
+        locs_html = list_group.find_all(class_='location')
+        open_html = list_group.find_all(class_='date')
+    except AttributeError:
+        pass
+    comps = name_comps(comps_html)
+    locs = get_locs(locs_html)
+    opens = get_opens(open_html)
+    comps_locs = list(zip(locs, comps, opens))
+    for item in comps_locs:
+        if item not in comps_locs_list:
+            comps_locs_list.append(item)
+            # print("adding", item, "from", url)
 
 
 # print(termcolor.colored(num_comps.string, 'blue', attrs=['bold']))
@@ -56,11 +71,10 @@ comps_locs = list(zip(locs, comps, opens))
 #     color = 'light_red' if item[2] is False else 'light_green'
 #     print(termcolor.colored(f'{item[1]} --- {item[0]}', color, attrs=['bold', 'concealed']))
 
-print(termcolor.colored(num_comps.string, 'blue', attrs=['bold']))
-print('----------------------------------------------------------\n')
-max_name_length = max(len(item[1]) for item in comps_locs)
 
-for item in comps_locs:
+max_name_length = max(len(item[1]) for item in comps_locs_list)
+
+for item in comps_locs_list:
     color = 'red' if item[2] is False else 'light_green'
     name = item[1].ljust(max_name_length)
     print(termcolor.colored(f'{name} --- {item[0]}', color, attrs=['bold']))
