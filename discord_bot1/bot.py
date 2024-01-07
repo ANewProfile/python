76f72ed77b92f227d703446b4572458f905c706d
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-01-07 13:31:34 -0500

    1/7/24 smth idk

--- discord_bot1/bot.py
+++ discord_bot1/bot.py
@@ -12,7 +12,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTEwNDUyNzgzODYwMzkyMzUyOA.GOEBvh.lm30-oYQYdXqVMuc3mpYvqtudIcw4eOlc-oJ_o'
+    TOKEN = 'MTEwNDUyNzgzODYwMzkyMzUyOA.GM5qg4.0gfi16bHhCX8zlbpeYii3duVZwS75Xqy2XsM2E'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
