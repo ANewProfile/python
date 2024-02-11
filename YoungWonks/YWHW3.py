76d1df4c0123f23c0f91233ef8204c24a7755421
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-02-11 13:22:08 -0500

    2/5/24 YoungWonks Pygame Homework 3 - Snake Game

--- YoungWonks/YWHW3.py
+++ YoungWonks/YWHW3.py
@@ -1,9 +1,10 @@
 import pygame
+import random
 pygame.init()
 
 WIDTH = 720
 HEIGHT = 720
-FRAMERATE = 10
+FRAMERATE = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 WHITE = (255, 255, 255)
@@ -15,27 +16,52 @@ STEP_W = WIDTH // 9
 STEP_H = HEIGHT // 9
 
 class Player(pygame.Rect):
-    def __init__(self, snake, path, right=False, left=False, up=False, down=False):
+    def __init__(self, snake, right=False, left=False, up=False, down=False):
         self.length = len(snake)
         self.head_x = snake[0][0]
         self.head_y = snake[0][1]
+        pygame.Rect.__init__(self, self.head_x, self.head_y, STEP_W, STEP_H)
         self.snake = snake
-        self.path = path
         self.go_right = right
         self.go_left = left
         self.up = up
         self.down = down
     
+    def move(self, x, y):
+        # 1. Second item becomes the value of first item
+        # 2. Third item becomes the value of fourth item
+        # 3. Repeat until you make last item the value of second to last item
+        # 4. Set first item to where you're going next
+
+        # self.move_ip(x, y)
+        # for i in range(1, len(self.snake)):
+        #     self.snake[i] = self.snake[i-1]
+        # self.snake[0] = [self.snake[0][0]+x, self.snake[0][1]+y]
+
+        self.move_ip(x, y)
+        if len(self.snake) > 1:
+            self.snake = self.snake[:-1]
+            self.snake.insert(0, [self.snake[0][0]+x, self.snake[0][1]+y])
+        else:
+            self.snake[0] = [self.snake[0][0]+x, self.snake[0][1]+y]
+    
     def draw(self):
         for square in self.snake:
             pygame.draw.rect(window, LIGHT_GREEN, (square[0], square[1], STEP_W, STEP_H))
-        
-        # pygame.draw.rect(window, LIGHT_GREEN, (self.head_x, self.head_y, STEP_W, STEP_H))
+    
+    def elongate(self):
+        if self.up:
+            self.snake.append([self.snake[-1][0], self.snake[-1][1]+STEP_W])
+        elif self.down:
+            self.snake.append([self.snake[-1][0], self.snake[-1][1]-STEP_W])
+        elif self.right:
+            self.snake.append([self.snake[-1][0]+STEP_H, self.snake[-1][1]])
+        else:
+            self.snake.append([self.snake[-1][0]-STEP_H, self.snake[-1][1]])
 
 class Fruit(pygame.Rect):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        pygame.Rect.__init__(self, x, y, STEP_W, STEP_H)
 
     def draw(self):
         pygame.draw.rect(window, RED, (self.x, self.y, STEP_W, STEP_H)) 
@@ -56,7 +82,7 @@ score = 0
 
 running = True
 clock = pygame.time.Clock()
-player = Player([[player_x, player_y]], [])
+player = Player([[player_x, player_y]])
 fruit = Fruit(fruit_x, fruit_y)
 while running:
     clock.tick(FRAMERATE)
@@ -86,24 +112,18 @@ while running:
                 player.down = False
 
     if player.up is True:
-        player_y -= STEP_H
-        player.snake[0][1] = player_y
+        player.move(0, -STEP_H)
     elif player.down is True:
-        player_y += STEP_H
-        player.snake[0][1] = player_y
+        player.move(0, STEP_H)
     elif player.go_left is True:
-        player_x -= STEP_W
-        player.snake[0][0] = player_x
+        player.move(-STEP_W, 0)
     else:
-        player_x += STEP_W
-        player.snake[0][0] = player_x
+        player.move(STEP_W, 0)
     
-    if player.colliderect(fruit):  # Not working- ask Jayson
-        # Move the fruit to a non-snake position and add to score
-        # Elongate the snake
+    if player.colliderect(fruit):
+        fruit.update(random.randint(0, 8)*STEP_W, random.randint(0, 8)*STEP_H, STEP_W, STEP_H)
+        player.elongate()
         score += 1
-        print(f'Player picked up fruit! Score: {score}')
-        pass
     
     window.fill(BLACK)
     draw_grid()
@@ -112,4 +132,5 @@ while running:
 
     pygame.display.update()
 
+print(player.snake)
 pygame.quit()
