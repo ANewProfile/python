4436c2f9024377f7b678b02454b80d380e8e0f1d
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-03-24 17:17:31 -0400

    finished platformer game

--- YoungWonks/platformer_game/platformer_game.py
+++ YoungWonks/platformer_game/platformer_game.py
@@ -11,6 +11,7 @@ WHITE = (255, 255, 255)
 LIGHT_GREEN = (144, 238, 144)
 RED = (255, 0, 0)
 BLUE = (173, 216, 230)
+YELLOW = (255, 255, 0)
 
 
 image_width = 60
@@ -118,8 +119,8 @@ level_one = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 
 level_two = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -153,201 +154,239 @@ level_two = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-board = level_two
+board = level_one
 
 def convert_index_to_coords(i, j):  # i = row, j = column
     return (j*20, i*20)
 
-collision_platforms = []
-middle_grass_platforms = []
-left_grass_platforms = []
-right_grass_platforms = []
-finish = []
-for row in range(len(board)):
-    for column in range(len(board[row])):
-        if board[row][column] == 1:
-            coords = convert_index_to_coords(row, column)
-            platform = pygame.Rect(coords[0], coords[1], 20, 20)
-            collision_platforms.append(platform)
-            if board[row][column+1] == 1 and board[row][column-1] != 1:
-                left_grass_platforms.append(platform)
-            elif board[row][column-1] == 1 and board[row][column+1] != 1:
-                right_grass_platforms.append(platform)
-            else:
-                middle_grass_platforms.append(platform)
-        elif board[row][column] == 2:
-            coords = convert_index_to_coords(row, column)
-            platform = pygame.Rect(coords[0], coords[1], 20, 20)
-            collision_platforms.append(platform)
-            finish.append(platform)
+def display_board(board):
+    collision_platforms = []
+    middle_grass_platforms = []
+    left_grass_platforms = []
+    right_grass_platforms = []
+    finish = []
+    for row in range(len(board)):
+        for column in range(len(board[row])):
+            if board[row][column] == 1:
+                coords = convert_index_to_coords(row, column)
+                platform = pygame.Rect(coords[0], coords[1], 20, 20)
+                collision_platforms.append(platform)
+                if board[row][column+1] == 1 and board[row][column-1] != 1:
+                    left_grass_platforms.append(platform)
+                elif board[row][column-1] == 1 and board[row][column+1] != 1:
+                    right_grass_platforms.append(platform)
+                else:
+                    middle_grass_platforms.append(platform)
+            elif board[row][column] == 2:
+                coords = convert_index_to_coords(row, column)
+                platform = pygame.Rect(coords[0], coords[1], 20, 20)
+                collision_platforms.append(platform)
+                finish.append(platform)
+    
+    floor = pygame.Rect(0, HEIGHT, WIDTH, 1)
+    left_wall = pygame.Rect(-1, 0, 1, HEIGHT)
+    right_wall = pygame.Rect(WIDTH, 0, 1, HEIGHT)
+    ceiling = pygame.Rect(0, -1, WIDTH, 1)
 
+    collision_platforms.append(floor)
+    collision_platforms.append(left_wall)
+    collision_platforms.append(right_wall)
+    collision_platforms.append(ceiling)
 
-with open('YoungWonks/platformer_game/save.txt', 'r') as save_file:
-    line = save_file.readline()
-    # print(line)
-    if line:
-        data = line.split(' ')
-        # print(data)
-        for num in range(len(data)):
-            # print(data[num])
-            data[num] = int(data[num])
-        character_rect = Character(data[0], data[1], data[2], data[3])
-    else:
-        character_rect = Character(0, HEIGHT-image_height, image_width, image_height)
-
-current_list = idle_right
+    return collision_platforms, left_grass_platforms, right_grass_platforms, middle_grass_platforms, finish
 
 # if platform.collide_point(hitbox.left, hitbox.bottom+1) or platform.collide_point(hitbox.right, hitbox.bottom+1):
 #    DO NOT JUMP
 
 bg_rect = pygame.Rect(0, 0, WIDTH, HEIGHT)
 
-floor = pygame.Rect(0, HEIGHT, WIDTH, 1)
-left_wall = pygame.Rect(-1, 0, 1, HEIGHT)
-right_wall = pygame.Rect(WIDTH, 0, 1, HEIGHT)
-ceiling = pygame.Rect(0, -1, WIDTH, 1)
-
-collision_platforms.append(floor)
-collision_platforms.append(left_wall)
-collision_platforms.append(right_wall)
-collision_platforms.append(ceiling)
-
 title_rect = pygame.Rect(0, 0, WIDTH, 100)
-start_rect = pygame.Rect(420, 300, WIDTH/4, 80)
-quit_rect = pygame.Rect(420, 390, WIDTH/4, 80)
+start_rect = pygame.Rect(0, 0, WIDTH/4, 80)
+start_rect.center = (540, 260)
+continue_rect = pygame.Rect(500, 390, WIDTH/4, 80)
+continue_rect.center = (540, 350)
+quit_rect = pygame.Rect(500, 480, WIDTH/4, 80)
+quit_rect.center = (540, 440)
 
 
 def display_start_screen():
-    pygame.draw.rect(window, LIGHT_GREEN, title_rect)
-    pygame.draw.rect(window, RED, start_rect)
-    pygame.draw.rect(window, BLUE, quit_rect)
+    start_screen = True
+    while start_screen:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                start_screen = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if quit_rect.collidepoint(pygame.mouse.get_pos()):
+                    start_screen = False
+                elif start_rect.collidepoint(pygame.mouse.get_pos()):
+                    start_screen = False
+                    with open('YoungWonks/platformer_game/save.txt', 'w') as save:
+                        save.write('')
+                    
+                    return game(level_one)
+                elif continue_rect.collidepoint(pygame.mouse.get_pos()):
+                    start_screen = False
+                    with open('YoungWonks/platformer_game/save.txt', 'r') as save:
+                        data = save.readline()
+                        if data:
+                            data = data.split(' ')
+                            if data[-1] == 'one':
+                                return game(level_one)
+                            else:
+                                return game(level_two)
 
-running = True
-clock = pygame.time.Clock()
-current_sprite = 0
-up_counter = 0
-jumped = True
-while running:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            with open('YoungWonks/platformer_game/save.txt', 'w') as save_file:
-                save_file.write(f'{character_rect.x} {character_rect.y} {character_rect.width} {character_rect.height}')
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
-                current_list = walk_right
-                current_sprite = 0
-                character_rect.moving_right = True
-            elif event.key == pygame.K_a:
-                current_list = walk_left
-                current_sprite = 0
-                character_rect.moving_left = True
-            elif event.key == pygame.K_SPACE and not jumped:
-                if not character_rect.moving_up:
-                    if current_list in (idle_left, walk_left):
-                        current_list = jump_left
-                    elif current_list in (idle_right, walk_right):
-                        current_list = jump_right
+        window.fill(BLACK)
+        window.blit(background, bg_rect)
+
+        pygame.draw.rect(window, LIGHT_GREEN, title_rect)
+        pygame.draw.rect(window, RED, start_rect)
+        pygame.draw.rect(window, YELLOW, continue_rect)
+        pygame.draw.rect(window, BLUE, quit_rect)
+
+        pygame.display.update()
+    
+    pygame.quit()
+
+
+def game(board):
+    with open('YoungWonks/platformer_game/save.txt', 'r') as save_file:
+        line = save_file.readline()
+        # print(line)
+        if line:
+            data = line.split(' ')
+            # print(data)
+            for num in range(len(data)-1):
+                # print(data[num])
+                data[num] = int(data[num])
+            character_rect = Character(data[0], data[1], data[2], data[3])
+        else:
+            character_rect = Character(0, HEIGHT-image_height, image_width, image_height)
+    collision_platforms, left_grass_platforms, right_grass_platforms, middle_grass_platforms, finish = display_board(board)
+    current_list = idle_right
+    running = True
+    clock = pygame.time.Clock()
+    current_sprite = 0
+    up_counter = 0
+    jumped = True
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                with open('YoungWonks/platformer_game/save.txt', 'w') as save_file:
+                    save_board = 'one' if board == level_one else 'two'
+                    save_file.write(f'{character_rect.x} {character_rect.y} {character_rect.width} {character_rect.height} {save_board}')
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    current_list = walk_right
                     current_sprite = 0
-                    up_counter = 0
-                    character_rect.moving_up = True
+                    character_rect.moving_right = True
+                elif event.key == pygame.K_a:
+                    current_list = walk_left
+                    current_sprite = 0
+                    character_rect.moving_left = True
+                elif event.key == pygame.K_SPACE and not jumped:
+                    if not character_rect.moving_up:
+                        if current_list in (idle_left, walk_left):
+                            current_list = jump_left
+                        elif current_list in (idle_right, walk_right):
+                            current_list = jump_right
+                        current_sprite = 0
+                        up_counter = 0
+                        character_rect.moving_up = True
+                    jumped = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    current_list = idle_right
+                    current_sprite = 0
+                    character_rect.moving_right = False
+                elif event.key == pygame.K_a:
+                    current_list = idle_left
+                    current_sprite = 0
+                    character_rect.moving_left = False
+        
+        if character_rect.moving_up:
+            character_rect.hitbox.move_ip(0, -5)
+            for platform in collision_platforms:
+                if character_rect.hitbox.colliderect(platform):
+                    character_rect.hitbox.top = platform.bottom
+            up_counter += 1
+        elif character_rect.moving_down:
+            last_y = character_rect.hitbox.top
+            character_rect.hitbox.move_ip(0, 5)
+            for platform in collision_platforms:
+                if character_rect.hitbox.colliderect(platform):
+                    character_rect.hitbox.bottom = platform.top
+                    jumped = False
+                    if platform in finish:
+                        if board is level_two:
+                            print('finished!')
+                            running = False
+                        else:
+                            with open('YoungWonks/platformer_game/save.txt', 'w') as save:
+                                save.write('')
+                            return game(level_two)
+
+            if last_y != character_rect.hitbox.top:
+                # print(last_y, character_rect.hitbox.top)
                 jumped = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_d:
+
+        if character_rect.moving_left:
+            character_rect.hitbox.move_ip(-5, 0)
+            for platform in collision_platforms:
+                if character_rect.hitbox.colliderect(platform):
+                    character_rect.hitbox.left = platform.right
+        elif character_rect.moving_right:
+            character_rect.hitbox.move_ip(5, 0)
+            for platform in collision_platforms:
+                if character_rect.hitbox.colliderect(platform):
+                    character_rect.hitbox.right = platform.left
+        
+        
+
+        character_rect.center = character_rect.hitbox.center
+
+        if up_counter >= (FPS * (3/4)):
+            character_rect.moving_up = False
+            character_rect.moving_down = True
+            if current_list == jump_right:
                 current_list = idle_right
-                current_sprite = 0
-                character_rect.moving_right = False
-            elif event.key == pygame.K_a:
+            elif current_list == jump_left:
                 current_list = idle_left
-                current_sprite = 0
-                character_rect.moving_left = False
-    
-    if character_rect.moving_up:
-        character_rect.hitbox.move_ip(0, -5)
-        if character_rect.hitbox.colliderect(ceiling):
-            character_rect.hitbox.top = ceiling.bottom
-        for platform in collision_platforms:
-            if character_rect.hitbox.colliderect(platform):
-                character_rect.hitbox.top = platform.bottom
-        up_counter += 1
-    elif character_rect.moving_down:
-        last_y = character_rect.hitbox.top
-        character_rect.hitbox.move_ip(0, 5)
-        if character_rect.hitbox.colliderect(floor):
-            character_rect.hitbox.bottom = floor.top
-            jumped = False
-            character_rect.moving_down = False
-        for platform in collision_platforms:
-            if character_rect.hitbox.colliderect(platform):
-                character_rect.hitbox.bottom = platform.top
-                jumped = False
-                if platform in finish:
-                    print('finished!')
-                    running = False
-
-        if last_y != character_rect.hitbox.top:
-            # print(last_y, character_rect.hitbox.top)
-            jumped = True
-
-    if character_rect.moving_left:
-        character_rect.hitbox.move_ip(-5, 0)
-        if character_rect.hitbox.colliderect(left_wall):
-            character_rect.hitbox.left = left_wall.right
-        for platform in collision_platforms:
-            if character_rect.hitbox.colliderect(platform):
-                character_rect.hitbox.left = platform.right
-    elif character_rect.moving_right:
-        character_rect.hitbox.move_ip(5, 0)
-        if character_rect.hitbox.colliderect(right_wall):
-            character_rect.hitbox.right = right_wall.left
-        for platform in collision_platforms:
-            if character_rect.hitbox.colliderect(platform):
-                character_rect.hitbox.right = platform.left
-    
-    
-
-    character_rect.center = character_rect.hitbox.center
-
-    if up_counter >= (FPS * (3/4)):
-        character_rect.moving_up = False
-        character_rect.moving_down = True
-        if current_list == jump_right:
-            current_list = idle_right
-        elif current_list == jump_left:
-            current_list = idle_left
-    
-    if not character_rect.moving_up and not character_rect.moving_left and not character_rect.moving_right:
-        character_rect.moving_down = True
+        
+        if not character_rect.moving_up and not character_rect.moving_left and not character_rect.moving_right:
+            character_rect.moving_down = True
 
 
-    window.fill(BLACK)
-    window.blit(background, bg_rect)
+        window.fill(BLACK)
+        window.blit(background, bg_rect)
 
-    for platform in middle_grass_platforms:
-        window.blit(middle_grass_image, platform)
+        for platform in middle_grass_platforms:
+            window.blit(middle_grass_image, platform)
 
-    for platform in left_grass_platforms:
-        window.blit(left_grass_image, platform)
+        for platform in left_grass_platforms:
+            window.blit(left_grass_image, platform)
 
-    for platform in right_grass_platforms:
-        window.blit(right_grass_image, platform)
+        for platform in right_grass_platforms:
+            window.blit(right_grass_image, platform)
 
-    for platform in finish:
-        window.blit(finish_image, platform)
+        for platform in finish:
+            window.blit(finish_image, platform)
 
-    window.blit(current_list[current_sprite%len(current_list)], character_rect)
+        window.blit(current_list[current_sprite%len(current_list)], character_rect)
 
-    # if jumped and the ground below the character is not air:
-    #     jumped = False
+        # if jumped and the ground below the character is not air:
+        #     jumped = False
 
-    current_sprite += 1
-    if current_list in (jump_left, jump_right) and current_sprite >= len(current_list):
-        current_sprite -= 1
+        current_sprite += 1
+        if current_list in (jump_left, jump_right) and current_sprite >= len(current_list):
+            current_sprite -= 1
 
-    # display_start_screen()
-    
-    pygame.display.update()
+        # display_start_screen()
+        
+        pygame.display.update()
+
+    pygame.quit()
 
 
-pygame.quit()
+display_start_screen()
