54c0b2e13aaeca929965d720821eb96a2cf6c327
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-04-01 19:22:19 -0400

    finished YoungWonks pygame homework part 1

--- YoungWonks/YWHW8-1.py
+++ YoungWonks/YWHW8-1.py
@@ -1,9 +1,34 @@
 import pygame
+import math
 pygame.init()
 
 WIDTH = 1080
 HEIGHT = 720
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+class Pentagon:
+    def __init__(self, x, y, color, radius):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.radius = radius
+
+    def draw(self):
+        pygame.draw.polygon(window, self.color, [(self.x-self.radius/2, self.y-self.radius), (self.x+self.radius/2, self.y-self.radius), (self.x+self.radius, self.y), (self.x, self.y+self.radius), (self.x-self.radius, self.y)])
+
+pentagon = Pentagon(WIDTH//2, HEIGHT//2, WHITE, 50)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    window.fill(BLACK)
+    pentagon.draw()
+    pygame.display.update()
 
 pygame.quit()
