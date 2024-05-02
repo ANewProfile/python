3e52a1903e38966eefb65c5b4071f74d0907d47e
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-05-01 21:09:02 -0400

    This was the most stupid bug in the world, fixed in the smartest way ever

--- YoungWonks/level3-sql/YWHW1-1.py
+++ YoungWonks/level3-sql/YWHW1-1.py
@@ -2,14 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect('YWHW1-1.db')
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS nutritionfacts (Name TEXT,ServingsContainer INTEGER,ServingSize INTEGER,Calories INTEGER,Expiration TEXT)')
+c.execute('CREATE TABLE IF NOT EXISTS nutritionfacts (Name TEXT,ServingsContainer INTEGER,ServingSize TEXT,Calories INTEGER,Expiration TEXT)')
 
-c.execute("INSERT INTO nutritionfacts VALUES ('Name', 1, 1, 1, '01/01/11')")
-c.execute("INSERT INTO nutritionfacts VALUES ('Name', 1, 1, 1, '01/01/11')")
-c.execute("INSERT INTO nutritionfacts VALUES ('Name', 1, 1, 1, '01/01/11')")
-c.execute("INSERT INTO nutritionfacts VALUES ('Name', 1, 1, 1, '01/01/11')")
-c.execute("INSERT INTO nutritionfacts VALUES ('Name', 1, 1, 1, '01/01/11')")
-c.execute("INSERT INTO nutritionfacts VALUES ('Name', 1, 1, 1, '01/01/11')")
+c.execute("INSERT INTO nutritionfacts VALUES ('Lactaid', 8, '1 cup', 130, '05/26/24')")
+c.execute("INSERT INTO nutritionfacts VALUES ('Iodized Sea Salt', 567, '0.25 tsp', 0, '-')")
+c.execute("INSERT INTO nutritionfacts VALUES ('Organic Peanut Butter', 16, '2 tbsp', 190, '12/17/24')")
+c.execute("INSERT INTO nutritionfacts VALUES ('Cereal', 9, '1 cup', 160, '02/12/25')")
+c.execute("INSERT INTO nutritionfacts VALUES ('Canola Oil', 63, '1 tbsp', 120, '06/29/25')")
 
 # c.execute('SELECT * FROM nutritionfacts')
 # food_items = c.fetchall()
