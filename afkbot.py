36d3a629820edf13b3e94f8a085b4c04d0d3da0d
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-06-03 07:52:52 -0400

    porkchop

--- afkbot.py
+++ afkbot.py
@@ -2,19 +2,36 @@ import pyautogui as pag
 import random
 import time
 
-curr_cords = pag.position()
-afk_counter = 0
-while True:
-    if pag.position() == curr_cords:
-        afk_counter += 1
-    else:
-        afk_counter = 0
-        curr_cords = pag.position()
+# curr_cords = pag.position()
+# afk_counter = 0
+# while True:
+#     if pag.position() == curr_cords:
+#         afk_counter += 1
+#     else:
+#         afk_counter = 0
+#         curr_cords = pag.position()
 
-    if afk_counter > 5:
-        x = random.randint(0, 2250)
-        y = random.randint(0, 1500)
-        pag.moveTo(x, y, 0.5)
-        curr_cords = pag.position()
-    print(f'AFK counter is: {afk_counter}')
-    time.sleep(2)
+#     if afk_counter > 5:
+#         x = random.randint(0, 1080)
+#         y = random.randint(0, 720)
+#         pag.moveTo(x, y, 0.5)
+#         curr_cords = pag.position()
+#     print(f'AFK counter is: {afk_counter}')
+#     time.sleep(2)
+
+def press_spacebar():
+    pag.press(' ')
+    
+# time.sleep(10)
+# pag.displayMousePosition()
+
+time.sleep(3)
+running = True
+while running:
+    screen = pag.screenshot()
+    pixel = screen.getpixel(pag.position())
+    print(pixel)
+    if (pixel[:-1]) == (182, 182, 182):
+        press_spacebar()
+    else:
+        print('Not white')
