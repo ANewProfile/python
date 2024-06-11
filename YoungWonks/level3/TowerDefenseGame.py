f8fd485773fedb7a481b44d98dac6857df1b68e8
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-06-10 21:29:58 -0400

    lots and lots of changes

--- YoungWonks/level3/TowerDefenseGame.py
+++ YoungWonks/level3/TowerDefenseGame.py
@@ -36,6 +36,16 @@ def create_save(game):
     c.execute("CREATE TABLE IF NOT EXISTS points (value INTEGER)")
     c.execute("CREATE TABLE IF NOT EXISTS allies (x INTEGER, y INTEGER, type TEXT)")
     c.execute("CREATE TABLE IF NOT EXISTS enemies (x INTEGER, y INTEGER, health INTEGER, type TEXT)")
+    
+    slinked_map: dict = game.linked_map
+    sset_map: list = game.set_map
+    savail_locs: list = game.avail_locs
+    spoints: int = game.points
+    sallies: list = game.allies
+    senemies: list = game.enemies
+    
+    for loc, tile_class in slinked_map.items():
+        c.execute("INSERT INTO linkedMap VALUES (?, ?, ?, ?, ?, ?)", (loc[0], loc[1], tile_class.previous.location[0], tile_class.previous.location[1], tile_class.next.location[0], tile_class.next.location[1]))
     # linked_map, set_map, avail_locs, points, allies, enemies
 
 def get_text(msg, size, color):
