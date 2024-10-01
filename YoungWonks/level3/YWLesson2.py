f2a099c98575b9ec55334f0202b0e84b7a26e688
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-09-30 21:49:45 -0400

    happy monday _ bleh _

--- YoungWonks/level3/YWLesson2.py
+++ YoungWonks/level3/YWLesson2.py
@@ -58,11 +58,48 @@ def find_appeared(cards, turn, inrow):
             
     return seen
 
+def start_new_round(cards, turn, order, playing):
+    num_appeared, most_appeared = find_most_appeared(cards, turn)
+    top = (num_appeared, most_appeared)
+    print(f'{turn} starts with {top}')
+    for _ in range(0, top[0]):
+        cards[turn].remove(top[1])
+        # cards[playing[turn-1]].remove(top[1])
+        
+    if cards[turn] == []:
+    # if cards[playing[turn-1]] == []:
+        order.append(str(turn))
+        cards.pop(turn)
+        playing.remove(turn)
+    
+    return cards, order, playing, top
+
+def play_next_move(cards, turn, order, playing, top):
+    next_options = find_appeared(cards, turn, top[0])
+    for option in next_options:
+        if option > top[1]:
+            top = (top[0], option)
+            print(f'{turn} plays {top}')
+            
+            for _ in range(0, top[0]):
+                cards[turn].remove(top[1])
+                # cards[playing[turn-1]].remove(top[1])
+            
+            if cards[turn] == []:
+            # if cards[playing[turn-1]] == []:
+                order.append(str(turn))
+                cards.pop(turn)
+                playing.remove(turn)
+            
+            break
+    
+    return cards, order, playing, top
+
 
 def main(input):
     cards = parse(input)
     fake_turn = 0
-    playing = list(range(1, len(cards)+1))
+    playing = list(range(1, len(cards)+1)) # list of all players with players removed as we play
     order = []
     
     old_top = (0, 0)
@@ -74,68 +111,41 @@ def main(input):
     new_round = True
     while True:
         # turn = (fake_turn%len(cards))+1
-        turn = playing[fake_turn%len(cards)]
+        print("cards are", cards)
+        cur_player_name = playing[fake_turn%len(cards)] # current player
         
         old_top = top
         
         if len(cards) <= 1:
             break
         
-        if first_pass == turn and passing:
+        if first_pass == cur_player_name and passing:
             new_round = True
-            fake_turn = (first_pass+3)-2
+            fake_turn = playing.index(first_pass)-1
             passing = False
             first_pass = 0
+            print(cur_player_name, "already passed, start new round")
             continue
         
         if new_round:
-            num_appeared, most_appeared = find_most_appeared(cards, turn)
-            top = (num_appeared, most_appeared)
-            print(f'{turn} sets {top}')
-            for _ in range(0, top[0]):
-                cards[turn].remove(top[1])
-                # cards[playing[turn-1]].remove(top[1])
-                
-            if cards[turn] == []:
-            # if cards[playing[turn-1]] == []:
-                order.append(str(turn))
-                cards.pop(turn)
-                playing.remove(turn)
-                
+            cards, order, playing, top = start_new_round(cards, cur_player_name, order, playing)
             fake_turn += 1
             new_round = False
             continue
+            
+        #time.sleep(0.5)
         
-        time.sleep(0.5)
-        
-        next_options = find_appeared(cards, turn, top[0])
-        for option in next_options:
-            if option > top[1]:
-                top = (top[0], option)
-                print(f'{turn} plays {top}')
-                passing = False
-                first_pass = 0
-                
-                for _ in range(0, top[0]):
-                    cards[turn].remove(top[1])
-                    # cards[playing[turn-1]].remove(top[1])
-                
-                if cards[turn] == []:
-                # if cards[playing[turn-1]] == []:
-                    order.append(str(turn))
-                    print(turn)
-                    cards.pop(turn)
-                    playing.remove(turn)
-                
-                break
+        cards, order, playing, top = play_next_move(cards, cur_player_name, order, playing, top)
         fake_turn += 1
         
         if old_top == top:
-            print('passed')
+            print(cur_player_name, 'passed')
             if not passing:
-                first_pass = turn
+                first_pass = cur_player_name
             passing = True
-            fake_turn += 1
+        else:
+            passing = False
+            first_pass = 0
     
     
     return ' '.join(order) + ' ' + str(list(cards.keys())[0])
@@ -144,5 +154,5 @@ answer = main('4 \n\
 6S 9C 9H QC 9S 5H KS TC 5C QH JS 5D JH \n\
 3C 7S 5S JD 8H QD TD 2C 6D TS 2D 3S 8D \n\
 TH AC 7H 6H 4D 4H 3D AD 7D 2H 8S 6C 4S \n\
-KC 2S 9D AS 7C KD KH 3H JC 4C QS 8C AH')
+KC 2S 9D AS 7C KD KH 3H JC 4C QS 8C AH')  # desired: 1 2 3 4
 print(answer)
