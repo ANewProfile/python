23d2b7efbd12045f469b67f69a2af43c21c519bb
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-11-01 06:57:12 -0400

    things

--- YoungWonks/level3/YWLesson6.py
+++ YoungWonks/level3/YWLesson6.py
@@ -1,3 +1,10 @@
+def get_left(node, structure, current_layer):
+    return structure[current_layer+1][structure[current_layer][(structure[current_layer].index(node)-1) * 2]] if len(structure[current_layer+1]) <= (structure[current_layer].index(node)-1) * 2 else None
+
+def get_right(node, structure, current_layer):
+    return structure[(structure[current_layer][(structure[current_layer].index(node)-1) * 2]) + 1]  if len(structure[current_layer+1]) <= ((structure[current_layer].index(node)-1) * 2) + 1 else None
+    
+
 def check_structure(structure):
     current_layer = 1
     num_layers = len(structure)
@@ -14,6 +21,13 @@ def check_structure(structure):
             
     return True
 
+def swap_values(first_node, second_node):
+    temp_value = first_node.value
+    first_node.value = second_node.value
+    second_node.value = temp_value
+    
+    return first_node, second_node
+
 class Node:
     def __init__(self, value, left, right):
         self.value = value
@@ -28,20 +42,29 @@ class Tree:
     def add(self, node: Node):
         depth_check = self.depth_on
         
+        
         self.structure[self.depth_on].append(node)
-        if ((len(self.structure[self.depth_on])-1) ** 2) == len(self.structure[self.depth_on]):
+        
+        parent = self.structure[self.depth_on-1][self.structure[self.depth_on].index(node)//2]
+        if get_left(parent, self.structure, self.depth_on-1) == node:
+            parent.left = node
+        if get_right(parent, self.structure, self.depth_on-1) == node:
+            parent.right = node
+            
+        if (2 ** (self.depth_on-1)) == len(self.structure[self.depth_on]):
             self.depth_on += 1
+            self.structure[self.depth_on] = []
         
         current_node = node
         while True:
             if check_structure(self.structure):
+                print('added %s' % (node.value))
                 break
             
             parent = self.structure[depth_check-1][self.structure[depth_check].index(current_node)//2]
+            print('swapping %s with %s' % (current_node.value, parent.value))
             if current_node.value <= parent.value:
-                temp_value = parent.value
-                parent.value = current_node.value
-                current_node.value = temp_value
+                parent, current_node = swap_values(parent, current_node)
                 
                 current_node = parent
                 depth_check -= 1
@@ -67,15 +90,11 @@ class Tree:
             
             if current_node.value >= current_node.right.value or current_node.value >= current_node.left.value:
                 if current_node.right.value >= current_node.left.value:
-                    temp_value = current_node.value
-                    current_node.value = current_node.left.value
-                    current_node.left.value = temp_value
+                    current_node, current_node.left = swap_values(current_node, current_node.left)
                     
                     current_node = current_node.left
                 else:
-                    temp_value = current_node.value
-                    current_node.value = current_node.right.value
-                    current_node.right.value = temp_value
+                    current_node, current_node.right = (current_node, current_node.right)
                     
                     current_node = current_node.right
     
@@ -86,13 +105,15 @@ class Tree:
                 printing.append(str(item.value))
             print(" ".join(printing))
         
-tree = Tree(Node(7, None, None))
-tree.add(Node(8, None, None))
-tree.add(Node(12, None, None))
-tree.view()
-print('-------------')
-tree.remove()
-tree.view()
-print('-------------')
-tree.add(Node(3, None, None))
+tree = Tree(Node('P', None, None))
+tree.add(Node('R', None, None))
+tree.add(Node('O', None, None))
+tree.add(Node('G', None, None))
+tree.add(Node('R', None, None))
+tree.add(Node('A', None, None))
+tree.add(Node('M', None, None))
+tree.add(Node('M', None, None))
+tree.add(Node('I', None, None))
+tree.add(Node('N', None, None))
+tree.add(Node('G', None, None))
 tree.view()
