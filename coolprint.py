aa05d2f2f084cd151cae4da0543b490135af641b
Theodore Chen <littletheo@mac.lan>
2025-01-09 08:48:05 -0500

    made better

--- coolprint.py
+++ coolprint.py
@@ -3,23 +3,22 @@ import time
 
 python_print = print
 python_input = input
-python_int = int
 
 
-def print(txt):
+def print(txt, speed=(random.randint(2, 8)/100)):
     text = str(txt)
     for c in text:
         python_print(c, end="", flush=True)
-        time.sleep(random.randint(2, 8)/100)
+        time.sleep(speed)
     python_print()
 
 
-def input(txt):
+def input(txt, speed=(random.randint(2, 8)/100)):
     text = str(txt)
     for c in text:
         python_print(c, end="", flush=True)
-        time.sleep(random.randint(2, 8)/100)
+        time.sleep(speed)
 
     go = python_input("")
-    python_print
+    python_print()
     return go
